119f98ac08d3e3c9d504c776aa2d3933c24c6230
itsravenous <tom@itsravenous.com>
2017-10-08 07:33:34 -0700

    Add sample package dependency
    So far, so good. Using a swift 3 package because the RxSwift
    package is swift 3 too.

--- Package.swift
+++ Package.swift
@@ -15,13 +15,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "3.6.0"),
+        .package(url: "https://github.com/itsravenous/MySwift3Package.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Dependencies",
-            dependencies: ["RxSwift"],
+            dependencies: ["RxSwift", "MySwift3Package"],
             path: ".deps-sources"
         )
     ]
