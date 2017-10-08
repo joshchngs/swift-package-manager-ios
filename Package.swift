4cb1e8fb615f31d7388d334655d830183a105ad0
itsravenous <tom@itsravenous.com>
2017-10-08 08:01:33 -0700

    Add sample swift 4 package dependency
    Seems to have worked same as the swift 3 package

--- Package.swift
+++ Package.swift
@@ -16,13 +16,14 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "3.6.0"),
         .package(url: "https://github.com/itsravenous/MySwift3Package.git", from: "1.0.0"),
+        .package(url: "https://github.com/itsravenous/MySwift4Package.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Dependencies",
-            dependencies: ["RxSwift", "MySwift3Package"],
+            dependencies: ["RxSwift", "MySwift3Package", "MySwift4Package"],
             path: ".deps-sources"
         )
     ]
